cec36dbd41019bd5e20b390146463a078c092bd6
Nate <Googeg89@gmail.com>
2021-09-14 21:27:52 -0400

    committ

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 from turtle import Turtle, Screen
 
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape('circle')
-screen = Screen()
-screen.exitonclick()
+tim = Turtle()
+tim.shape('turtle')
+tim.color('green')
+
+import random
+
+colors = ['firebrick', 'pink', 'lavender', 'deep pink', 'orange', 'chartreuse',
+          'purple', 'gray', 'dark blue']
+def draw_shape(num_of_sides):
+    angle = 360 / num_of_sides
+
+    for _ in range(num_of_sides):
+
+        tim.fd(100)
+        tim.right(angle)
 
 
 
+for i in range(3, 11):
+    tim.color(random.choice(colors))
+    draw_shape(i)
+
+
+s = Screen()
+s.exitonclick()
